6050ed9727a7053a9871c0f9485be16326e50879
Cathleen Vu <cathleendvu@gmail.com>
2025-01-12 16:01:03 -0800

    Initial Commit

---
+++ PacePal/PacePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacePalApp.swift
+//  PacePal
+//
+//  Created by Cathleen Vu on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PacePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
