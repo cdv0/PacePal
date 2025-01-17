66e26ea17961b61e29778d25dc9f8683ef473a46
Cathleen Vu <cathleendvu@gmail.com>
2025-01-17 14:29:54 -0800

    Add user struct to represent user data

---
+++ PacePal/Features/Authentication/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  PacePal
+//
+//  Created by Cathleen Vu on 1/17/25.
+//
+
+import Foundation
+
+struct User {
+    var username: String = ""
+    var password: String = ""
+    var email: String = ""
+}
