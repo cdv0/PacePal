a2c091d6bdd2dbb7f4ca931fb6b32ca5006e1629
Cathleen Vu <cathleendvu@gmail.com>
2025-01-17 14:51:12 -0800

    Create and design LoginView with navigation to ForgotPasswordView

---
+++ PacePal/Features/Authentication/Views/ForgotPasswordView.swift
@@ -0,0 +1,14 @@
+//
+//  ForgotPasswordView.swift
+//  PacePal
+//
+//  Created by Cathleen Vu on 1/17/25.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text("Filler")
+    }
+}
---
+++ PacePal/Features/Authentication/Views/LoginView.swift
@@ -0,0 +1,82 @@
+//
+//  LoginView.swift
+//  PacePal
+//
+//  Created by Cathleen Vu on 1/17/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var user = User()
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Spacer()
+            
+            Image("PacePalLogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 350, height: 100)
+            Text("Welcome to PacePal!")
+                .font(.title2.bold())
+                .padding(.bottom, 3)
+            Text("Track your steps")
+                .font(.system(size: 14))
+                .opacity(0.5)
+                .padding(.bottom, 40)
+            
+            VStack(spacing: 15) {
+                VStack (alignment: .leading) {
+                    Text("Email")
+                        .opacity(0.4)
+                    TextField("", text: $user.username)
+                }
+                .customTextFieldShape(color: .gray.opacity(0.11))
+                
+                VStack (alignment: .leading) {
+                    Text("Password")
+                        .opacity(0.4)
+                    TextField("", text: $user.password)
+                }
+                .customTextFieldShape(color: .gray.opacity(0.11))
+            }
+            .padding(.horizontal, 30)
+            .padding(.bottom, 25)
+            
+            Text("LOGIN")
+                .customTextFieldShape(color: Color(hex: 0x53A57D))
+                .bold()
+                .foregroundStyle(.white)
+            
+            NavigationLink(destination: ForgotPasswordView()) {
+                Text("Forgot password?")
+            }
+            .padding(10)
+            .foregroundStyle(Color(hex: 0x53A57D))
+            .font(.system(size: 15))
+            .bold()
+            
+            Spacer()
+            Spacer()
+            
+            Group {
+                Text("Don't have an account? ")
+                + Text("Register!")
+            }
+            .font(.system(size: 15))
+            .fontWeight(.medium)
+            
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(hex: 0xFBF7F4))
+        .ignoresSafeArea()
+        .preferredColorScheme(.light)
+    }
+}
+
+#Preview {
+    LoginView()
+}
