a23ad5e009e76515abbfa17401c4a715939c3e19
Cathleen Vu <cathleendvu@gmail.com>
2025-01-17 14:32:07 -0800

    Add view extension for custom text field shape

---
+++ PacePal/Utilities/Extensions/LoginPageExtensions.swift
@@ -0,0 +1,30 @@
+//
+//  LoginPageExtensions.swift
+//  PacePal
+//
+//  Created by Cathleen Vu on 1/17/25.
+//
+
+import SwiftUI
+
+struct CustomTextFieldShape: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .padding([.leading, .trailing], 25)
+            .padding([.top, .bottom], 30)
+            .frame(width: 350, height: 60)
+            .background(color)
+            .clipShape(.capsule)
+            .font(.system(size: 12))
+    }
+}
+
+extension View {
+    func customTextFieldShape(color: Color) -> some View {
+        modifier(CustomTextFieldShape(color: color))
+    }
+}
+
+
