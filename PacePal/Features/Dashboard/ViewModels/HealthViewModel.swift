e4474f4f5cd4291b8eb31fa051dbd2bc47c03fd3
Cathleen Vu <cathleendvu@gmail.com>
2025-01-22 16:50:05 -0800

    Initialize HealthViewModel with HealthKit authorization for step count, energy burned, and walking distance

--- PacePal/Features/Dashboard/ViewModels/HealthViewModel.swift
+++ PacePal/Features/Dashboard/ViewModels/HealthViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import HealthKit
+
+class HealthViewModel: ObservableObject {
+    let healthStore = HKHealthStore()
+    
+    init() {
+        let healthTypes: Set = [
+            HKQuantityType(.stepCount),
+            HKQuantityType(.activeEnergyBurned),
+            HKQuantityType(.distanceWalkingRunning),
+        ]
+        
+        Task {
+            do {
+                if !HKHealthStore.isHealthDataAvailable() {
+                    print("Health data is unavailable on the user's device.")
+                return
+                }
+                
+                try await healthStore.requestAuthorization(toShare: [], read: healthTypes)
+            } catch {
+                fatalError("DEBUG: Failed to fetch health data with error \(error.localizedDescription)")
+            }
+        }
+    }
+}
