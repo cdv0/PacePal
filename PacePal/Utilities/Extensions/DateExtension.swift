e090bc15b0d2744b61088b27eb1a019d1bf1f728
Cathleen Vu <cathleendvu@gmail.com>
2025-01-22 19:15:07 -0800

    Create extension to retrieve start of day

--- PacePal/Utilities/Extensions/DateExtension.swift
+++ PacePal/Utilities/Extensions/DateExtension.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+extension Date {
+    static var startOfDay: Date { // Access the type itself rather than making a Date() instance. We do this because the property is not specific to any single Date instance
+        Calendar.current.startOfDay(for: Date())
+    }
+}
