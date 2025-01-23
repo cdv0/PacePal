49f5cb1a5356cb83c6406c2769cb50997b4f0223
Cathleen Vu <cathleendvu@gmail.com>
2025-01-22 23:59:10 -0800

    Add HealthViewModel methods for fetching steps, distance, active time, and calories

--- PacePal/Features/Dashboard/ViewModels/HealthViewModel.swift
+++ PacePal/Features/Dashboard/ViewModels/HealthViewModel.swift
@@ -9,6 +9,11 @@ import Foundation
 import HealthKit
 
 class HealthViewModel: ObservableObject {
+    @Published var steps: Steps = Steps(count: 0, date: Date())
+    @Published var calories: Calories = Calories(count: 0, date: Date())
+    @Published var distance: Distance = Distance(count: 0, date: Date())
+    @Published var activeTime: ActiveTime = ActiveTime(seconds: 0, date: Date())
+    
     let healthStore = HKHealthStore()
     
     init() {
@@ -16,18 +21,112 @@ class HealthViewModel: ObservableObject {
             HKQuantityType(.stepCount),
             HKQuantityType(.activeEnergyBurned),
             HKQuantityType(.distanceWalkingRunning),
+
         ]
         
         Task {
             do {
                 if !HKHealthStore.isHealthDataAvailable() {
-                    print("Health data is unavailable on the user's device.")
+                    print("DEBUG: Health data is unavailable on the user's device.")
                 return
                 }
                 
-                try await healthStore.requestAuthorization(toShare: [], read: healthTypes)
+                try await healthStore.requestAuthorization(toShare: [], read: healthTypes) // toShare specifies the types of health data your app wants to write to the health app. read specifies the type of health data your apap wants to read form the health app. Both accept a set.
             } catch {
                 fatalError("DEBUG: Failed to fetch health data with error \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func fetchSteps(from startDate: Date, to endDate: Date, interval: DateComponents) async throws {
+        guard let stepsType = HKQuantityType.quantityType(forIdentifier: .stepCount) else { return } // Creates an instance of HKQuantityType and gets the *type* of the data we're querying for
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, // Creates a predicate (filter) the health data you're querying for based on certain criteria. .strictStartDate ensures the start date is inclusive.
+                                                    end: endDate,
+                                                    options: .strictStartDate)
+        let query = HKSamplePredicate.quantitySample(type: stepsType, // Extracts specific data from the predicate
+                                                     predicate: predicate)
+        let stepsQuery = HKStatisticsCollectionQueryDescriptor(predicate: query, // Describes the query to be executed. This aggregates step count data. It doesn't retrieve the data
+                                                               options: .cumulativeSum,
+                                                               anchorDate: startDate,
+                                                               intervalComponents: interval)
+        let stepsCount = try await stepsQuery.result(for: healthStore) // Fetches the steps
+        
+        stepsCount.enumerateStatistics(from: startDate, to: endDate) { statistics, _ in
+            let count = statistics.sumQuantity()?.doubleValue(for: .count()) ?? 0
+            let step = Steps(count: Int(count), date: statistics.startDate) // Use the interval's start date for each result
+            
+            DispatchQueue.main.async { // Ensures the UI is updated on the main thread
+                self.steps = step
+            }
+        }
+    }
+    
+    // TODO: If user doesn't have an apple watch, try to estimate calories burned based on user stats
+    func fetchCalories(from startDate: Date, to endDate: Date, interval: DateComponents) async throws {
+        guard let calorieType = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned) else { return }
+        let predicate = HKQuery.predicateForSamples(withStart: startDate,
+                                                    end: endDate,
+                                                    options: .strictStartDate)
+        let query = HKSamplePredicate.quantitySample(type: calorieType,
+                                                     predicate: predicate)
+        let calorieQuery = HKStatisticsCollectionQueryDescriptor(predicate: query,
+                                                                 options: .cumulativeSum,
+                                                                 anchorDate: startDate,
+                                                                 intervalComponents: interval)
+        let calorieCount = try await calorieQuery.result(for: healthStore)
+        
+        calorieCount.enumerateStatistics(from: startDate, to: endDate) { statistics, _ in
+            let count = statistics.sumQuantity()?.doubleValue(for: .kilocalorie()) ?? 0
+            let calories = Calories(count: Int(count), date: statistics.startDate)
+            
+            DispatchQueue.main.async {
+                self.calories = calories
+            }
+        }
+    }
+    
+    func fetchDistance(from startDate: Date, to endDate: Date, interval: DateComponents) async throws {
+        guard let distanceType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning) else { return }
+        let predicate = HKQuery.predicateForSamples(withStart: startDate,
+                                                    end: endDate,
+                                                    options: .strictStartDate)
+        let query = HKSamplePredicate.quantitySample(type: distanceType,
+                                                     predicate: predicate)
+        let distanceQuery = HKStatisticsCollectionQueryDescriptor(predicate: query,
+                                                                 options: .cumulativeSum,
+                                                                 anchorDate: startDate,
+                                                                 intervalComponents: interval)
+        let distanceCount = try await distanceQuery.result(for: healthStore)
+        
+        distanceCount.enumerateStatistics(from: startDate, to: endDate) { statistics, _ in
+            let count = statistics.sumQuantity()?.doubleValue(for: .mile()) ?? 0
+            let distance = Distance(count: Double(count), date: statistics.startDate)
+            
+            DispatchQueue.main.async {
+                self.distance = distance
+            }
+        }
+    }
+    
+    func fetchActiveTime(from startDate: Date, to endDate: Date, interval: DateComponents) async throws {
+        guard let activeTimeType = HKQuantityType.quantityType(forIdentifier: .appleExerciseTime) else { return }
+        let predicate = HKQuery.predicateForSamples(withStart: startDate,
+                                                    end: endDate,
+                                                    options: .strictStartDate)
+        let query = HKSamplePredicate.quantitySample(type: activeTimeType,
+                                                     predicate: predicate)
+        let activeTimeQuery = HKStatisticsCollectionQueryDescriptor(predicate: query,
+                                                                 options: .cumulativeSum,
+                                                                 anchorDate: startDate,
+                                                                 intervalComponents: interval)
+        let activeTimeCount = try await activeTimeQuery.result(for: healthStore)
+        
+        activeTimeCount.enumerateStatistics(from: startDate, to: endDate) { statistics, _ in
+            let count = statistics.sumQuantity()?.doubleValue(for: .second()) ?? 0
+            let activeTime = ActiveTime(seconds: Int(count), date: statistics.startDate)
+            
+            DispatchQueue.main.async {
+                self.activeTime = activeTime
             }
         }
     }
