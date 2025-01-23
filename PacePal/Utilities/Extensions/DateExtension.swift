a8cea601f83271a05375305eb103748605f4e825
Cathleen Vu <cathleendvu@gmail.com>
2025-01-22 23:57:23 -0800

    Add endOfDay property to Date extension

--- PacePal/Utilities/Extensions/DateExtension.swift
+++ PacePal/Utilities/Extensions/DateExtension.swift
@@ -9,6 +9,12 @@ import Foundation
 
 extension Date {
     static var startOfDay: Date { // Access the type itself rather than making a Date() instance. We do this because the property is not specific to any single Date instance
-        Calendar.current.startOfDay(for: Date())
+        Calendar.current.startOfDay(for: Date()) // Returns 00:00:00 of that day
+    }
+    
+    static var endOfDay: Date {
+        // ! operator is specifically used to unwrap optionals and extract their underlying value
+        let startOfNextDay = Calendar.current.date(byAdding: .day, value: 1, to: Calendar.current.startOfDay(for: Date()))! // Gives 00:00:00 of the next day from adding 1
+        return Calendar.current.date(byAdding: .second, value: -1, to: startOfNextDay)! // Gives 23:59:59
     }
 }
