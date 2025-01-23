10c765ec36789bedd3df7dc25eb8d502c0caf478
Cathleen Vu <cathleendvu@gmail.com>
2025-01-22 16:53:31 -0800

    Update ContentView to show PacePalTabView if user object exists

--- PacePal/Features/Root/ContentView.swift
+++ PacePal/Features/Root/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     var body: some View {
         Group {
             if authViewModel.userSession != nil {
-                ProfileView()
+                PacePalTabView()
             } else {
                 LoginView()
             }
